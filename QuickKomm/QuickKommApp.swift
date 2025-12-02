76beabce1f7f38c125e89baf2e58309e24436dc2
Ravi Ranjan <ravi.ranjan@pw.live>
2025-12-03 00:02:06 +0530

    Initial Commit

---
+++ QuickKomm/QuickKommApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickKommApp.swift
+//  QuickKomm
+//
+//  Created by Ravi Ranjan on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickKommApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickKommTests/QuickKommTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickKommTests.swift
+//  QuickKommTests
+//
+//  Created by Ravi Ranjan on 03/12/25.
+//
+
+import Testing
+@testable import QuickKomm
+
+struct QuickKommTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
